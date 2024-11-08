10fea8e2fe9aa4bd6b83b6b93ddd8fdff0fab0a3
SunnyKumar-code <sk202542@gmail.com>
2024-11-08 13:16:31 +0530

    make

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let joke = document.querySelector('p');
+let joke = document.querySelector('.joke');
 let getBtn = document.querySelector('button');
+
 
 async function getJoke() {
     joke.innerText = "Loading..."; 
     let header = {
-        headers: { 'X-Api-Key': 'VDcV5lUlgPP1X3kCkfSQog==QgeCVnr0Mcu7a5Rp' }
+        headers: { 'X-Api-Key': 'VDcV5lUlgPP1X3kCkfSQog==QgeCVnr0Mcu7a5Rp' },
     };
-    let url = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
+    let url = 'https://api.api-ninjas.com/v1/dadjokes';
     
     try {
+        
         let res = await fetch(url, header);
         let data = await res.json();
+        console.log(data);
         joke.innerText = `${data[0].joke} 😂😂` ;
     } catch (error) {
+        console.log(error)
         joke.innerText = "Oops! Could not load a joke.";
         console.error("Error fetching joke:", error);
     }
